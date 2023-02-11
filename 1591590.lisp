0a645a55841e9b2cb656a0649d6c99f2b4cc9fdf
Aidan Horemans <aidanjhoremans@gmail.com>
2023-02-10 21:42:39 -0700

    cleanup of interpreter

--- 1591590.lisp
+++ 1591590.lisp
@@ -1,6 +1,130 @@
+(defun fl-interp (E P)
+    (xeval E P nil nil)
+)
+
+(defun xeval (E P N V) ; E = expression, P = program, N = names, V = values
+    ; iterate through
+    (cond 
+        ((null E) nil)
+        ((numberp E) E)
+        ((atom E) ; when given a bound variable (like X), this fetches and returns the value from V
+            (xassoc E N V)
+        )
+    (t ; from above cond
+        (let ((name (car E)) ; function name for current value in E
+              (e1 (car (cdr E)))
+              (e2 (car (cdr (cdr E))))
+              (e3 (car (cdr (cdr (cdr E))))))
+        (cond
+            ((eq name 'if)
+                (if (xeval e1 P N V)
+                    (xeval e2 P N V)
+                    (xeval e3 P N V)
+                )
+            )
+            ((member name '(null atom eq first rest cons equal car cdr number + - * > < = and or not)) ; built in functions!
+                (let ((ev-e1 (xeval e1 P N V)) ; evaluate their arguments first
+                      (ev-e2 (xeval e2 P N V)))
+                    (run-atomic name ev-e1 ev-e2)
+                )
+            )
+            (t ; check against P if name is a function that has been user defined
+                (run-function name E P N V)
+            ))
+        )
+    )) 
+)
+
+(defun run-atomic (name ev-e1 ev-e2)
+    (cond
+        ((eq name 'null)
+            (null ev-e1)
+        )
+        ((eq name 'atom)
+            (atom ev-e1)
+        )
+        ((eq name 'eq)
+            (eq ev-e1 ev-e2)
+        )
+        ((eq name 'first)
+            (car ev-e1)
+        )
+        ((eq name 'rest)
+            (cdr ev-e1)
+        )
+        ((eq name 'cons)
+            (cons ev-e1 ev-e2)
+        )
+        ((eq name 'equal)
+            (equal ev-e1 ev-e2)
+        )
+        ((eq name 'car)
+            (car ev-e1)
+        )
+        ((eq name 'cdr)
+            (cdr ev-e1)
+        )
+        ((eq name 'number)
+            (numberp ev-e1)
+        )
+        ((eq name '+)
+            (+ ev-e1 ev-e2)
+        )
+        ((eq name '-)
+            (- ev-e1 ev-e2)
+        )
+        ((eq name '*)
+            (* ev-e1 ev-e2)
+        )
+        ((eq name '>)
+            (> ev-e1 ev-e2)
+        )
+        ((eq name '<)
+            (< ev-e1 ev-e2)
+        )
+        ((eq name '=)
+            (= ev-e1 ev-e2)
+        )
+        ((eq name 'and)
+            (and ev-e1 ev-e2)
+        )
+        ((eq name 'or)
+            (or ev-e1 ev-e2)
+        )
+        ((eq name 'not)
+            (not ev-e1)
+        )
+    )
+)
+
+(defun run-function (name E P N V)
+    (let ((body (locate-function name P))
+            (param-list (locate-parameters name P))
+    )
+        (if body ; the user defined it, evaluate function call
+            (let ((evaluated-list (evlist (cdr E) P N V)) ; evaluate the list of values for the function call
+            )  
+                (let ((new-names (cons param-list N)) ; construct a context of names consisting of the function's parameter list and the names of all other values
+                        (new-values (cons evaluated-list V)) ;construct a context of new-values, consisting of the evaluted list of values in front of all other values V
+                )
+                    (xeval body P new-names new-values)
+                )
+            )
+            (evlist E P N V) ; otherwise, return as evaluated list since not a primitive and not a known function call -> different from above evaluated-list
+        )
+    )
+)
+
+(defun evlist (arg-list P N V)
+    (if (null arg-list)
+        nil
+        (cons (xeval (car arg-list) P N V) (evlist (cdr arg-list) P N V)) ; construct a list of the evaluation of (car arg-list), with the rest of arg-list evaluated
+    )
+)
+
 (defun xassoc (e N V)
     (if (null N)
-        nil
+        e ; if not in context, just return the value as though it was quoted
         (if (member e (car N))
             (locate e (car N) (car V)) ; locate e in N
             (xassoc e (cdr N) (cdr V)) ; iterate over 
@@ -36,142 +160,11 @@
     )
 )
 
-(defun closure (arg-list body n v) ; f (X Y) = (+ X Y) -> f (1 2) | (X Y) = arg-list, 
-    (cons (cons arg-list body) (cons n v))
-)
-
-(defun closure-parameters (c)
-    (car (car c))
-)
-
-(defun closure-body (c)
-    (cdr (car c))
-)
-
-(defun closure-values (c)
-    (cdr (cdr c))
-)
-
-(defun closure-names (c)
-    (car (cdr c))
-)
-
-
-(defun xeval (E P N V) ; E = expression, P = program, N = names, V = values
-    ; iterate through
-    (cond 
-        ((null E) nil)
-        ((numberp E) E)
-        ((or (atom E)) ; when given a bound variable (like X), this fetches and returns the value from V
-            (xassoc E N V)
-        )
-    (t ; from above cond
-        (let 
-        ((name (car E)) ; function name for current value in E
-        (e1 (car (cdr E)))
-        (e2 (car (cdr (cdr E))))
-        (e3 (car (cdr (cdr (cdr E))))))
-        (cond
-            ((eq name 'if)
-                (if (xeval e1 P N V)
-                    (xeval e2 P N V)
-                    (xeval e3 P N V)
-                )
-            )
-            ((member name '(null atom eq first rest cons equal car cdr number + - * > < = and or not)) ; built in funtions!
-                (let ((ev-e1 (xeval e1 P N V)) ; evaluate their arguments
-                      (ev-e2 (xeval e2 P N V)))
-                (cond 
-                    ((eq name 'null)
-                        (null ev-e1)
-                    )
-                    ((eq name 'atom)
-                        (atom ev-e1)
-                    )
-                    ((eq name 'eq)
-                        (eq ev-e1 ev-e2)
-                    )
-                    ((eq name 'first)
-                        (car ev-e1)
-                    )
-                    ((eq name 'rest)
-                        (cdr ev-e1)
-                    )
-                    ((eq name 'cons)
-                        (cons ev-e1 ev-e2)
-                    )
-                    ((eq name 'equal)
-                        (equal ev-e1 ev-e2)
-                    )
-                    ((eq name 'car)
-                        (car ev-e1)
-                    )
-                    ((eq name 'cdr)
-                        (cdr ev-e1)
-                    )
-                    ((eq name 'number)
-                        (numberp ev-e1)
-                    )
-                    ((eq name '+)
-                        (+ ev-e1 ev-e2)
-                    )
-                    ((eq name '-)
-                        (- ev-e1 ev-e2)
-                    )
-                    ((eq name '*)
-                        (* ev-e1 ev-e2)
-                    )
-                    ((eq name '>)
-                        (> ev-e1 ev-e2)
-                    )
-                    ((eq name '<)
-                        (< ev-e1 ev-e2)
-                    )
-                    ((eq name '=)
-                        (= ev-e1 ev-e2)
-                    )
-                    ((eq name 'and)
-                        (and ev-e1 ev-e2)
-                    )
-                    ((eq name 'or)
-                        (or ev-e1 ev-e2)
-                    )
-                    ((eq name 'not)
-                        (not ev-e1)
-                    )
-                ))
-            )
-        (t ; check against P if name is a function that has been user defined
-            (let ((body (locate-function name P))
-            (param-list (locate-parameters name P))
-            )
-                (if body ; the user defined it, evaluate function call
-                    (let ((evaluated-list (evlist (cdr E) P N V))
-                          (clos (closure param-list body N V))) ; do we need this?
-                        (let ((new-names (cons (closure-parameters clos) (closure-names clos)))
-                              (new-values (cons evaluated-list (closure-values clos))))
-                            (xeval body P new-names new-values) ; evaluate the body of the function
-                        )
-                    )
-                    ;E
-                    ;(xeval (car E) P N V)
-                    (evlist E P N V) ; otherwise, return as list since not a primitive and not a known function call
-                )
-            )
-        )
-        )
+; for numeric comparison methods, check if vals are numeric, if not then deal with them appropriately
+(defun numeric-atomic (F e1 e2)
+    (cond
+        (not (numberp e1) )
+        ()
+        (t ())
     )
-    )
-    ) 
-)
-
-(defun evlist (arg-list P N V)
-    (if (null arg-list)
-        nil
-        (cons (xeval (car arg-list) P N V) (evlist (cdr arg-list) P N V)) ; construct a list of the evaluation of (car arg-list), with the rest of arg-list evaluated
-    )
-)
-
-(defun fl-interp (E P)
-    (xeval E P nil nil)
 )
